b1f79f5779b81aed4008d42098cc8fd3e2795c94
fimitaka shimizu <gilgala.aiglos@gmail.com>
2020-10-08 17:55:29 +0900

    controller,edit,newの共通処理

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,5 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:edit, :show, :update]
   def index
     @blogs = Blog.all
   end
@@ -17,15 +18,12 @@ class BlogsController < ApplicationController
   end
 
   def edit
-    @blog = Blog.find(params[:id])
   end
 
   def show
-    @blog = Blog.find(params[:id])
   end
 
   def update
-    @blog = Blog.find(params[:id])
     if @blog.update(blog_params)
       redirect_to new_blog_path, notice: "ブログを編集しました！"
     else
@@ -36,5 +34,9 @@ class BlogsController < ApplicationController
   private
   def blog_params
     params.require(:blog).permit(:content)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
