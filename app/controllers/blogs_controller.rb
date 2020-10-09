a50d0ab5e05098aa06aeb44445f4481a09f17a50
fimitaka shimizu <gilgala.aiglos@gmail.com>
2020-10-09 15:27:21 +0900

    投稿確認画面作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,10 +6,14 @@ class BlogsController < ApplicationController
 
   def create
     @blog = Blog.new(blog_params)
-    if @blog.save
-      redirect_to blogs_path, notice: "ブログを投稿しました！"
-    else
+    if params[:back]
       render :new
+    else
+      if @blog.save
+        redirect_to blogs_path, notice: "ブログを投稿しました！"
+      else
+        render :new
+      end
     end
   end
 
@@ -34,6 +38,11 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     redirect_to blogs_path, notice: "ブログを削除しました！"
+  end
+
+  def confirm
+    @blog = Blog.new(blog_params)
+    render :new if @blog.invalid?
   end
 
   private
