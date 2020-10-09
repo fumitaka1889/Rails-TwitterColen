07bec10a53b74783881da648e15966e2915ea4ac
fimitaka shimizu <gilgala.aiglos@gmail.com>
2020-10-09 19:12:21 +0900

    header,footer,topページ作成と投稿一覧のレイアウト調整

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,8 +2,8 @@ module BlogsHelper
   def choose_new_or_edit
     if action_name == 'new' || action_name == 'create'
       confirm_blogs_path
-    else action_name == 'edit'
-      blogs_path
+    elsif action_name == 'edit'
+      blog_path
     end
   end
 end
